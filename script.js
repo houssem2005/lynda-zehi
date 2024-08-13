81b0f11b62c4d727db489578dab820d707d84164
lynda <46058993+houssem2005@users.noreply.github.com>
2024-08-13 20:44:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let cont2 = document.querySelector('.container2');
+let cont3 = document.querySelector('.container3');
+let cont4 = document.querySelector('.container4');
+let cont5 = document.querySelector('.container5');
+let cont6 = document.querySelector('.container6');
+let cont7 = document.querySelector('.container7');
+let cont8 = document.querySelector('.container8');
+function start(){
+    cont2.style.display= 'flex';
+    cont3.style.display= 'block';
+    cont4.style.display= 'block';
+    cont5.style.display= 'block';
+    cont6.style.display= 'block';
+    cont7.style.display= 'block';
+    cont8.style.display= 'flex';
+}
